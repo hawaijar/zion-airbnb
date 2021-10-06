68c40f1c70287dd7f1772101729fcc24aad54922
hawaijar <hawaijar.js@gmail.com>
2021-10-06 15:57:44 +0530

    Added few more dependencies

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native';
+import 'react-native-gesture-handler';
 import GuestScreen from './src/screens/Guest';
 // import LocationSearch from './src/screens/LocationSearch';
 const App = () => {
   return (
     <SafeAreaView>
       {/*<LocationSearch />*/}
-      <GuestScreen />
+      {/*<GuestScreen />*/}
     </SafeAreaView>
   );
 };
